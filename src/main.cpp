de08bc31923af112a656a00746bbf7a8f979238a
Johannes Doll <doll.johannes.94@gmail.com>
2020-02-02 14:14:41 +0100

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello World");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
